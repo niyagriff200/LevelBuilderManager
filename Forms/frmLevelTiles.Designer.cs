0eac02840825ff72e3babf491caf155ae7ba22d6
niyagriff200 <130501997+niyagriff200@users.noreply.github.com>
2026-04-14 10:05:50 -0500

    Added Level Tiles Table

--- Forms/frmLevelTiles.Designer.cs
+++ Forms/frmLevelTiles.Designer.cs
@@ -29,6 +29,17 @@
         private void InitializeComponent()
         {
             btnReturn = new Button();
+            lbTileCount = new Label();
+            btnDeleteLevelTile = new Button();
+            btnAddLevelTile = new Button();
+            lbTileNameID = new Label();
+            lbLevelNameID = new Label();
+            dgvLevelTilesManager = new DataGridView();
+            numTileCount = new NumericUpDown();
+            cmbLevelNameID = new ComboBox();
+            cmbTileNameID = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)dgvLevelTilesManager).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numTileCount).BeginInit();
             SuspendLayout();
             // 
             // btnReturn
@@ -39,21 +50,122 @@
             btnReturn.TabIndex = 1;
             btnReturn.Text = "Return to Menu";
             btnReturn.UseVisualStyleBackColor = true;
-            btnReturn.Click += this.btnReturn_Click;
+            btnReturn.Click += btnReturn_Click;
+            // 
+            // lbTileCount
+            // 
+            lbTileCount.AutoSize = true;
+            lbTileCount.Location = new Point(26, 454);
+            lbTileCount.Name = "lbTileCount";
+            lbTileCount.Size = new Size(153, 41);
+            lbTileCount.TabIndex = 29;
+            lbTileCount.Text = "Tile Count";
+            // 
+            // btnDeleteLevelTile
+            // 
+            btnDeleteLevelTile.Location = new Point(241, 638);
+            btnDeleteLevelTile.Name = "btnDeleteLevelTile";
+            btnDeleteLevelTile.Size = new Size(189, 58);
+            btnDeleteLevelTile.TabIndex = 27;
+            btnDeleteLevelTile.Text = "Delete Level Tile";
+            btnDeleteLevelTile.UseVisualStyleBackColor = true;
+            // 
+            // btnAddLevelTile
+            // 
+            btnAddLevelTile.Location = new Point(13, 638);
+            btnAddLevelTile.Name = "btnAddLevelTile";
+            btnAddLevelTile.Size = new Size(189, 58);
+            btnAddLevelTile.TabIndex = 26;
+            btnAddLevelTile.Text = "Add Level Tiles";
+            btnAddLevelTile.UseVisualStyleBackColor = true;
+            // 
+            // lbTileNameID
+            // 
+            lbTileNameID.AutoSize = true;
+            lbTileNameID.Location = new Point(26, 295);
+            lbTileNameID.Name = "lbTileNameID";
+            lbTileNameID.Size = new Size(151, 41);
+            lbTileNameID.TabIndex = 25;
+            lbTileNameID.Text = "Tile Name";
+            // 
+            // lbLevelNameID
+            // 
+            lbLevelNameID.AutoSize = true;
+            lbLevelNameID.Location = new Point(26, 146);
+            lbLevelNameID.Name = "lbLevelNameID";
+            lbLevelNameID.Size = new Size(172, 41);
+            lbLevelNameID.TabIndex = 24;
+            lbLevelNameID.Text = "Level Name";
+            // 
+            // dgvLevelTilesManager
+            // 
+            dgvLevelTilesManager.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvLevelTilesManager.Location = new Point(585, 82);
+            dgvLevelTilesManager.Name = "dgvLevelTilesManager";
+            dgvLevelTilesManager.RowHeadersWidth = 102;
+            dgvLevelTilesManager.Size = new Size(1080, 703);
+            dgvLevelTilesManager.TabIndex = 21;
+            // 
+            // numTileCount
+            // 
+            numTileCount.Location = new Point(26, 524);
+            numTileCount.Name = "numTileCount";
+            numTileCount.Size = new Size(300, 47);
+            numTileCount.TabIndex = 30;
+            // 
+            // cmbLevelNameID
+            // 
+            cmbLevelNameID.FormattingEnabled = true;
+            cmbLevelNameID.Location = new Point(24, 202);
+            cmbLevelNameID.Name = "cmbLevelNameID";
+            cmbLevelNameID.Size = new Size(302, 49);
+            cmbLevelNameID.TabIndex = 31;
+            // 
+            // cmbTileNameID
+            // 
+            cmbTileNameID.FormattingEnabled = true;
+            cmbTileNameID.Location = new Point(24, 359);
+            cmbTileNameID.Name = "cmbTileNameID";
+            cmbTileNameID.Size = new Size(302, 49);
+            cmbTileNameID.TabIndex = 32;
             // 
             // frmLevelTiles
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1165, 764);
+            ClientSize = new Size(1677, 798);
+            Controls.Add(cmbTileNameID);
+            Controls.Add(cmbLevelNameID);
+            Controls.Add(numTileCount);
+            Controls.Add(lbTileCount);
+            Controls.Add(btnDeleteLevelTile);
+            Controls.Add(btnAddLevelTile);
+            Controls.Add(lbTileNameID);
+            Controls.Add(lbLevelNameID);
+            Controls.Add(dgvLevelTilesManager);
             Controls.Add(btnReturn);
             Name = "frmLevelTiles";
             Text = "Level Builder Manager - Level Tiles";
+            Load += frmLevelTiles_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvLevelTilesManager).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numTileCount).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button btnReturn;
+        private Label lbTileCount;
+        private CheckBox cbCanBeWalkedOn;
+        private Button btnDeleteLevelTile;
+        private Button btnAddLevelTile;
+        private Label lbTileNameID;
+        private Label lbLevelNameID;
+        private TextBox txtTypeEntry;
+        private DataGridView dgvLevelTilesManager;
+        private NumericUpDown numTileCount;
+        private ComboBox cmbLevelNameID;
+        private ComboBox cmbTileNameID;
     }
 }
--- Forms/frmLevelTiles.cs
+++ Forms/frmLevelTiles.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace LevelBuilderManager
 {
@@ -29,6 +30,31 @@ namespace LevelBuilderManager
             //When the return button is clicked, close this form (which will trigger the FormClosed event and show the main menu)
             frmOriginal.Show();
             this.Close();
+        }
+
+        private void frmLevelTiles_Load(object sender, EventArgs e)
+        {
+            string connLevelTiles = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = LevelTilesDatabase; Integrated Security = True; " +
+                "Connect Timeout = 30; Encrypt = True; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False; Command Timeout = 30;";
+
+            using SqlConnection sqlLevelTilesConn = new SqlConnection(connLevelTiles);
+            {
+                //Open the connection to the database
+                sqlLevelTilesConn.Open();
+
+                //Query to select all records from the LevelTiles table
+                string sqlLevelTilesQuery = "SELECT * FROM LevelTiles";
+
+                // Create a SqlDataAdapter to execute the query and fill a DataTable
+                SqlDataAdapter sqlLevelTileAdapter = new SqlDataAdapter(sqlLevelTilesQuery, sqlLevelTilesConn);
+
+                //Fill a DataTable with the results of the query
+                DataTable dtLevelTiles = new DataTable();
+                sqlLevelTileAdapter.Fill(dtLevelTiles);
+
+                //Bind the DataTable to the DataGridView to display the tiles
+                dgvLevelTilesManager.DataSource = dtLevelTiles;
+            }
         }
     }
 }
